33b93a3102a8d7a15c0460bd4bfdba429ba7ae41
DamianValle <damianvs@kth.se>
2021-04-28 13:25:49 +0200

    batchnorm implemented

--- 3-cifar10_klayer/ann.py
+++ 3-cifar10_klayer/ann.py
@@ -19,11 +19,13 @@ class ANN:
         self.epochs = 300
         self.lr = 1e-5
         self.jitter = False
+        self.batch_norm = True
 
         self.lr_min = 1e-5
         self.lr_max = 1e-2
         self.ns = -1
         self.num_cycles = 2
+        self.alpha = 0.7
 
         self.h_nodes = [50, 30, 20, 20, 10, 10, 10, 10]
         self.num_layers = len(self.h_nodes)
@@ -31,6 +33,19 @@ class ANN:
         self.plot = True
 
         self.init_weights_and_biases()
+        self.init_gamma_and_beta()
+
+    def init_gamma_and_beta(self):
+        self.gamma = [None] * (self.num_layers)
+        self.gamma = [np.random.normal(self.gauss_mean, np.sqrt(2/self.h_nodes[i]), (self.h_nodes[i], 1)) for i in range(self.num_layers)]
+
+        self.beta = [None] * (self.num_layers + 1)
+        for i in range(self.num_layers):
+            self.beta[i] = np.zeros((self.h_nodes[i], 1))
+        self.beta[self.num_layers] = np.zeros((self.k, 1))
+
+        self.mu_avg = None
+        self.var_avg = None
 
     def init_weights_and_biases(self):
         """
@@ -55,9 +70,6 @@ class ANN:
         for i in range(self.num_layers):
             self.b[i] = np.zeros((self.h_nodes[i], 1))
         self.b[self.num_layers] = np.zeros((self.k, 1))
-
-        for i in self.w:
-            print(i.shape)
 
     def train(self, x_train, y_train, x_val, y_val, x_test, y_test):
         num_batches = int(x_train.shape[1] / self.batch_size)
@@ -85,12 +97,8 @@ class ANN:
                 #if self.jitter:
                 #    x_batch += 0.01 * np.random.randn(*x_batch.shape)
 
-                grad_w, grad_b = self.compute_gradients(x_batch, y_batch)
-
-                # meter esto a un lambda o algo
-                for k in range(self.num_layers+1):
-                    self.w[k] -= self.lr * grad_w[k]
-                    self.b[k] -= self.lr * grad_b[k]
+                grad_w, grad_b, grad_beta, grad_gamma = self.compute_gradients(x_batch, y_batch)
+                self.parameter_step(grad_w, grad_b, grad_beta, grad_gamma)
 
                 t += 1
                 if t == self.ns * self.num_cycles + 1:
@@ -115,13 +123,26 @@ class ANN:
             print("Epoch ", str(i+1), " val acc: ", str(self.compute_accuracy(x_val, y_val)))
 
         print("TRAINING FINISHED")
-        print("Test accuracy: ", self.compute_accuracy(x_test, y_test))
+        print("Test accuracy: ", self.compute_accuracy(x_test, y_test, 'test'))
 
         if self.plot:
             self.plot_graphs(train_acc_hist, train_cost_hist, val_acc_hist, val_cost_hist)
 
-    def update_mu_std_avg(self, mu, std):
+    def parameter_step(self, grad_w, grad_b, grad_beta, grad_gamma):
+
+        if self.batch_norm:
+            for i in range(self.num_layers):
+                self.beta[i] = self.beta[i] - self.lr * grad_beta[i]
+                self.gamma[i] = self.gamma[i] - self.lr * grad_gamma[i]
+
+        for i in range(self.num_layers+1):
+            self.w[i] -= self.lr * grad_w[i]
+            self.b[i] -= self.lr * grad_b[i]
+
+
+    def update_mu_var_avg(self, mu, var):
         self.mu_avg = mu if self.mu_avg == None else [self.alpha * self.mu_avg[l] + (1 - self.alpha) * mu[l] for l in range(len(mu))]
+        self.var_avg = var if self.var_avg == None else [self.alpha * self.var_avg[l] + (1 - self.alpha) * var[l] for l in range(len(var))]
 
     def cyclical_lr(self, t):
         l = int(t / (2*self.ns))
@@ -157,35 +178,100 @@ class ANN:
         return np.exp(x) / np.sum(np.exp(x), axis=0)
 
     def evaluate_classifier(self, X):
-        """
-        w: k x d
-        X: d x n
-        b: k x 1
-
-        output: n x k
-        """
-
-        act_h = [None] * (self.num_layers)
-        s = [None] * (self.num_layers+1)
+        act_h =     [None] * (self.num_layers)
+        s =         [None] * (self.num_layers + 1)
+        norm_s =  [None] * (self.num_layers)
+        final_s =   [None] * (self.num_layers)
+        mu =        [None] * (self.num_layers)
+        var =       [None] * (self.num_layers)
 
         # first layer
         s[0] = np.dot(self.w[0], X) + self.b[0]
-        act_h[0] = np.maximum(0, s[0])
+
+        if self.batch_norm:
+            #mu[0] = np.sum(s[0], axis = 1) / s[0].shape[1]
+            mu[0] = np.mean(s[0], axis = 1)
+            #var[0] = 1/ s[0].shape[1] * np.sum(((s[0].T - mu[0]).T)**2, axis = 1)
+            var[0] = np.var(s[0], axis = 1)
+            norm_s[0] = self.batch_normalize(s[0], mu[0], var[0])
+            final_s[0] = self.gamma[0] * norm_s[0] + self.beta[0]
+
+            act_h[0] = np.maximum(0, final_s[0])
+        else:
+            act_h[0] = np.maximum(0, s[0])
 
         # hidden layers
         for i in range(1, self.num_layers):
             s[i] = np.dot(self.w[i], act_h[i-1]) + self.b[i]
-            act_h[i] = np.maximum(0, s[i])
+
+            if self.batch_norm:
+                #mu[i] = 1/s[i].shape[1] * np.sum(s[i], axis = 1)
+                mu[i] = np.mean(s[i], axis = 1)
+                #var[i] = 1/ s[i].shape[1] * np.sum(((s[i].T - mu[i]).T)**2, axis = 1)
+                var[i] = np.var(s[i], axis = 1)
+                norm_s[i] = self.batch_normalize(s[i], mu[i], var[i])
+                final_s[i] = self.gamma[i] * norm_s[i] + self.beta[i]
+
+                act_h[i] = np.maximum(0, final_s[i])
+            else:
+                act_h[i] = np.maximum(0, s[i])
 
         # last layer
         s[self.num_layers] = np.dot(self.w[self.num_layers], act_h[self.num_layers-1]) + self.b[self.num_layers]
 
         p = self.softmax(s[self.num_layers])
 
-        return p, act_h, s
+        if self.batch_norm:
+            self.update_mu_var_avg(mu, var)
+
+        return p, act_h, s, norm_s, mu, var
+
+    def evaluate_classifier_test(self, X):
+        act_h =     [None] * (self.num_layers)
+        s =         [None] * (self.num_layers + 1)
+        norm_s =  [None] * (self.num_layers)
+        final_s =   [None] * (self.num_layers)
+        mu =        [None] * (self.num_layers)
+        var =       [None] * (self.num_layers)
+
+        # first layer
+        s[0] = np.dot(self.w[0], X) + self.b[0]
+
+        if self.batch_norm:
+
+            norm_s[0] = self.batch_normalize(s[0], self.mu_avg[0], self.var_avg[0])
+            final_s[0] = self.gamma[0] * norm_s[0] + self.beta[0]
+
+            act_h[0] = np.maximum(0, final_s[0])
+        else:
+            act_h[0] = np.maximum(0, s[0])
+
+        # hidden layers
+        for i in range(1, self.num_layers):
+            s[i] = np.dot(self.w[i], act_h[i-1]) + self.b[i]
+
+            if self.batch_norm:
+                norm_s[i] = self.batch_normalize(s[i], self.mu_avg[i], self.var_avg[i])
+                final_s[i] = self.gamma[i] * norm_s[i] + self.beta[i]
+
+                act_h[i] = np.maximum(0, final_s[i])
+            else:
+                act_h[i] = np.maximum(0, s[i])
+
+        # last layer
+        s[self.num_layers] = np.dot(self.w[self.num_layers], act_h[self.num_layers-1]) + self.b[self.num_layers]
+        p = self.softmax(s[self.num_layers])
+
+        return p
+
+    def batch_normalize(self, s, mu, var):
+        part1 = np.diag(np.power((var + 1e-6),(-1/2)))
+        part2 = (s.T - mu).T
+        
+        return np.dot(part1, part2)
 
     def compute_cost(self, X, y_true):
-        y_pred, _, _ = self.evaluate_classifier(X)
+        y_pred, _, _, _, _, _ = self.evaluate_classifier(X)
 
         reg_term = sum(list(map(lambda x: np.sum(x**2), self.w)))
 
@@ -197,15 +283,18 @@ class ANN:
 
         return c_entropy
 
-    def compute_accuracy(self, X, y_true):
+    def compute_accuracy(self, X, y_true, split='train'):
         """
         Computes the accuracy of the classifier for a given set of samples and their ground truth labels.
 
         X: d x n
         y_true, y_pred: k x n
         """
+        if split == 'train':
+            y_pred, _, _, _, _, _ = self.evaluate_classifier(X)
+        elif split == 'test':
+            y_pred = self.evaluate_classifier_test(X)
 
-        y_pred, _, _ = self.evaluate_classifier(X)
         match = len(np.where(np.argmax(y_true, axis=0) == np.argmax(y_pred, axis=0))[0])
 
         return match/y_true.shape[1]
@@ -217,10 +306,12 @@ class ANN:
         """
         size = y_true.shape[1]
 
-        grad_b = [None]*(self.num_layers+1)
-        grad_w = [None]*(self.num_layers+1)
+        grad_b = [None] * (self.num_layers+1)
+        grad_w = [None] * (self.num_layers+1)
+        grad_gamma = [None] * (self.num_layers)
+        grad_beta = [None] * (self.num_layers)
 
-        y_pred, act_h, s = self.evaluate_classifier(X)
+        y_pred, act_h, s, norm_s, mu, var = self.evaluate_classifier(X)
 
         g_batch = y_pred - y_true
 
@@ -242,13 +333,21 @@ class ANN:
         g_batch = g_batch * h_act_ind
 
         for l in reversed(range(self.num_layers)):
+
+            if self.batch_norm:
+                grad_gamma[l] = np.dot((g_batch * norm_s[l]), np.ones(size)).reshape(-1,1) / size
+                grad_beta[l] = np.dot(g_batch, np.ones(size)).reshape(-1,1) / size
+
+                g_batch *= np.dot(self.gamma[l], np.ones((size,1)).T)
+                g_batch = self.batchnorm_backward(g_batch, s[l], mu[l], var[l], size)
+
             if l == 0:
                 grad_w[l] = np.dot(g_batch, X.T) / size + 2 * self.lamda*self.w[l]
             else:
                 grad_w[l] = np.dot(g_batch, act_h[l-1].T) / size + 2 * self.lamda * self.w[l]
 
             #grad_b[l] = np.sum(g_batch, axis=1).reshape(-1, 1) / size
-            grad_b[l] = np.dot(g_batch, np.ones((self.batch_size,1))) / size
+            grad_b[l] = np.dot(g_batch, np.ones((size,1))) / size
 
             if l > 0:
                 g_batch = np.dot(self.w[l].T, g_batch)
@@ -262,7 +361,19 @@ class ANN:
                             h_act_ind[k, j] = 1
                 g_batch = g_batch * h_act_ind
 
-        return grad_w, grad_b
+        return grad_w, grad_b, grad_beta, grad_gamma
+
+    def batchnorm_backward(self, g_batch, s_batch, mu, var, size):
+        sigma_1 = ((var + 1e-6)**(-0.5)).T
+        sigma_2 = ((var + 1e-6)**(-1.5)).T
+        bigG1 = g_batch * np.outer(sigma_1, np.ones((size,1)))
+        bigG2 = g_batch * np.outer(sigma_2, np.ones((size,1)))
+        bigD = s_batch - np.outer(mu, np.ones((size,1)))
+        c = np.dot((bigG2 * bigD), np.ones((size,1)))
+        g_batch = bigG1 - 1/size * np.dot(bigG1, np.ones((size,1)))
+        g_batch -= 1 / size * (bigD * np.outer(c, np.ones((size))))
+
+        return g_batch
 
     def check_gradients(self, X, y_true):
         grad_w1, _, _, _ = self.compute_gradients(X, y_true)
